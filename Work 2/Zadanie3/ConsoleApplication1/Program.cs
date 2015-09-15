094584c9140215bccc754c393592c0352fbc39ec
Tochilkin Misha <36104@mail.ru>
2015-09-15 20:12:02 +1000

    Add Zadanie 3-4 and report

---
+++ Work 2/Zadanie3/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int a;
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b;
+            b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("Большее число " + a);
+            }
+            else
+            {
+                Console.WriteLine("Большее число " + b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Work 2/Zadanie4/ConsoleApplication3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int a;
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b;
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье число");
+            int c;
+            c = int.Parse(Console.ReadLine());
+            if (a > b && a > c)
+            {
+                Console.WriteLine("Большее число " + a);
+            }
+            if (b > c && b > a)
+            {
+                Console.WriteLine("Большее число " + b);
+            }
+            if (c > b && c > a)
+            {
+                Console.WriteLine("Большее число " + c);
+            }
+                Console.ReadKey();
+            }
+        }
+    } 
