f23825135477046640148994bf8bf73cd970470e
Tochilkin Misha <36104@mail.ru>
2015-09-21 16:44:16 +1000

    Add work 3 zadanie 3

---
+++ Work 3/Zadanie3/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите числа, для подсчета суммы напишите 'эпарнишастопэ': ");
+            int a;
+            string s;
+            int summ = 0;
+            do
+            {
+                Console.Write("Число = ");
+                s = Console.ReadLine();
+                if (s == "эпарнишастопэ")
+                {
+                    s = "эпарнишастопэ";
+                }
+                else
+                {
+                    a = Convert.ToInt32(s);
+                    summ = summ + a;
+                }
+            }
+            while (s!= "эпарнишастопэ");
+            Console.Write(summ);
+            Console.ReadKey();
+        }
+    }
+}
