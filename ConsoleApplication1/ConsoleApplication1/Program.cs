06c85654aeac9da8124bfa39b5500d5aa0750779
Tochilkin Misha <36104@mail.ru>
2015-09-21 20:20:15 +1000

    Update code

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.Write("Введите n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i != n; i = i + 1) ;
+            Console.Write(i + 1);
+            Console.ReadKey();
+        }
+    }
+}
