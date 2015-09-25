4354b00bdce239ca434ccf88f8b949e2385ac774
Tochilkin Misha <36104@mail.ru>
2015-09-25 19:07:33 +1000

    add work4

---
+++ Work 4/Zadanie2/kek_app/kek_app/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kek_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов в массиве = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы задали массив состоящий из " + n + " элементов");
+            int[] massiv = new int[n];
+            int len = massiv.Length;
+            Random rnd = new Random();
+            for (int i = 0; i < n; i = i + 1)
+            {
+                int random_number = rnd.Next(-5, 20);
+                massiv[i] = random_number;
+            };
+            Console.WriteLine();
+            Console.WriteLine("Сгенерированный массив: ");
+            for (int z = 0; z < len; z = z + 1)
+            {
+                Console.Write(massiv[z] + " ");
+            };
+            Console.WriteLine();
+            Console.WriteLine("Массив составленный из ТОЛЬКО ЧЕТНЫХ элементов: ");
+            for (int r = 0; r < len; r = r + 1)
+            {
+                if (massiv[r]%2==0)
+                {
+                  Console.Write(massiv[r] + " ");
+                };
+            };
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Work 4/Zadanie4/stosik/stosik/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stosik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введити длинну массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] massiv = new int[n];
+            int massivD = massiv.Length;
+            Random random = new Random();
+            Console.WriteLine();
+            Console.WriteLine("Сгенерированный массив: ");
+            for (int i = 0; i < massivD; i++)
+            {
+                massiv[i] = random.Next(-6, 25);
+                Console.Write(massiv[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Меняем части массива местами: ");
+            for (int i = massivD - 1; -1 < i; i--)
+            {
+                Console.Write(massiv[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
